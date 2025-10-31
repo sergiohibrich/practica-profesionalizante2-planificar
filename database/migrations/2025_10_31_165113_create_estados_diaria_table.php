c4e5cae3cdb1d81233b9c5ab384a9ffd867c3ffa
Nacgo Web <baumgatnern@gmail.com>
2025-10-31 14:59:05 -0300

    migraciones de estado_diaria y planificacion_diaria

---
+++ database/migrations/2025_10_31_165113_create_estados_diaria_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estados_diaria', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('estado',45);
+            $table->date('fecha');
+
+            $table->unsignedBigInteger('planificacion_diaria_id');
+            $table->foreign('planificacion_diaria_id') ->references('id') ->on('planificacion_diaria');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estados_diaria');
+    }
+};
