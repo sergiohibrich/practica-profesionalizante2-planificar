cdb2949184df2c649f5e5de379b22f61e28d08e6
Juana Molina <juanamolina854@outlook.com>
2025-11-06 15:03:32 -0300

    controlador y rutas

---
+++ app/Http/Controllers/PersonasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PersonasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return DB::table('personas')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        DB::table('personas')->insert([
+            'apellidos'=> $request['apellidos'],
+            'nombres'=> $request['nombres'],
+            'dni' => $request['dni'],
+            '`e-mail`' => $request['e-mail'],
+            'telefono' => $request['telefono'],
+            'direccion' => $request['direccion'],
+            'fecha_nacimiento' => $request['fecha_nacimiento']
+        ]);
+        return response()->json(['mensaje'=>'Persona agregada correctamente']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return DB::table('personas')->where('id','=',$id)->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        DB::table('personas')->where('id', '=', $id)->update([
+        'apellidos' => $request['apellidos'],
+        'nombres' => $request['nombres'],
+        'dni' => $request['dni'],
+        '`e-mail`' => $request['e-mail'],
+        'telefono' => $request['telefono'],
+        'direccion' => $request['direccion'],
+        'fecha_nacimiento' => $request['fecha_nacimiento']
+    ]);
+
+    return response()->json(['mensaje' => 'Persona actualizada correctamente']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        DB::table('personas')->where('id', '=', $id)->delete();
+
+    return response()->json(['mensaje' => 'Persona eliminada correctamente']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonasController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/personas',[PersonasController::class, 'index']);
+Route::get('/personas/{id}',[PersonasController::class, 'show']);
+Route::post('/personas', [PersonaController::class, 'store']);
+Route::put('/personas/{id}', [PersonaController::class, 'update']);
+Route::delete('/personas/{id}', [PersonaController::class, 'destroy']);
