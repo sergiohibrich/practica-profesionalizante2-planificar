40c843941b216eebc129d1c5421f739e630dfadc
nicorsss <nicoreisnl@gmail.com>
2025-11-07 13:43:47 -0300

    Controlador y Rutas Api de Cursos

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursosController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cursos', [CursosController::class,'index']);
+Route::get('/cursos/{id}', [CursosController::class,'show']);
+Route::post('/cursos', [CursosController::class,'store']);
+Route::put('/cursos/{id}', [CursosController::class,'update']);
+Route::delete('/cursos/{id}', [CursosController::class,'destroy']);
