6e385ab8e846ff2b6d49d2e504432ddc878e50b3
Daniel Frías <friasdaniel996@gmail.com>
2025-11-12 16:33:12 -0300

    creación de controladores y rutas api para tabla cursados

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CursadosController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('cursados', [CursadosController::class, 'index']);
+Route::get('cursados/{id}', [CursadosController::class, 'show']);
+Route::put('cursados/{id}', [CursadosController::class, 'update']);
+Route::delete('cursados/{id}', [CursadosController::class, 'destroy']);
+Route::post('cursados', [CursadosController::class, 'store']);
