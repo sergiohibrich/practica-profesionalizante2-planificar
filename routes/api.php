952fe5a1ee44d149ea8207a69e95ad2be5329d2e
Gastón Azula <azulagaston250@gmail.com>
2025-11-13 17:42:03 -0300

    agrego areas y estados anual

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\areasController;
+use App\Http\Controllers\estados_anualController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('areas', areasController::class);
+Route::apiResource('estados_anual',estados_anualController::class);
