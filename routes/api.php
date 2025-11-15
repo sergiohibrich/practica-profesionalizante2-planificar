44b33f6d8c137caa23027fe71430471b7aca94f9
JohannaMiranda <mirandajohanna88@gmail.com>
2025-11-15 01:19:37 -0300

    route put->update

--- routes/api.php
+++ routes/api.php
@@ -41,4 +41,4 @@ use App\Http\Controllers\CargosController;
 Route::get('/sit_revista', [CargosController::class, 'index']);
 Route::get('/cargos/{id}', [CargosController::class, 'show']);
 Route::post('/cargos', [CargosController::class, 'store']);
-
+Route::put('/cargos/{id}', [CargosController::class, 'update']);
