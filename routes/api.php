ab6a3f74c0748c9e4c050e8555ba2ffbe8a89bce
JohannaMiranda <mirandajohanna88@gmail.com>
2025-11-16 22:06:32 -0300

    se agrega Rutas sit_revista

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,11 @@ Route::get('/cursos/{id}', [CursosController::class,'show']);
 Route::post('/cursos', [CursosController::class,'store']);
 Route::put('/cursos/{id}', [CursosController::class,'update']);
 Route::delete('/cursos/{id}', [CursosController::class,'destroy']);
+
+use App\Http\Controllers\SitRevistaController;
+
+Route::get('/sit_revista', [SitRevistaController::class, 'index']);
+Route::get('/sit_revista/{id}', [SitRevistaController::class, 'show']);
+Route::post('/sit_revista', [SitRevistaController::class, 'store']);
+Route::put('/sit_revista/{id}', [SitRevistaController::class, 'update']);
+Route::delete('/sit_revista/{id}', [SitRevistaController::class, 'destroy']);
